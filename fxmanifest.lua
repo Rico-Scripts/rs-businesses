49bf0f6dd84808925d60bea1ac136dcb3f22ace2
rico <r03292760@gmail.com>
2026-08-23 11:05:19 +0200

    feat: add prop-safe work animations for every NPC role

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Rico Scripts'
 description 'Uitgebreid beheer voor spelerwinkels en tankstations'
-version '1.1.1'
+version '1.1.2'
 
 sql_file 'sql/install.sql'
 rs_sql 'sql/install.sql'
